70144a3a36e6ab27fa15bcbe4ce55c96ef989620
Amit Pattanaik <161040099+amitPattanaik987@users.noreply.github.com>
2024-11-13 16:25:43 +0530

    Update Problems.jsx

--- src/Components/Problems/Problems.jsx
+++ src/Components/Problems/Problems.jsx
@@ -8,7 +8,7 @@ export default function Problems({ hackathon_name }) {
   const navigate = useNavigate();
 
   useEffect(() => {
-    fetch("http://localhost:3000/problems", {
+    fetch("https://hackathon-app-9hq6.onrender.com/problems", {
       method: "POST",
       headers: {
         "Accept": "application/json",
