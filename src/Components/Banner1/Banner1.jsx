9a82640f55094ea487843b4b18a35d68f7a1c6b6
Amit Pattanaik <161040099+amitPattanaik987@users.noreply.github.com>
2024-12-30 19:45:15 +0530

    Update Banner1.jsx

--- src/Components/Banner1/Banner1.jsx
+++ src/Components/Banner1/Banner1.jsx
@@ -12,7 +12,7 @@ export default function Banner1() {
         if (login) {
             const newemail = localStorage.getItem("email");
 
-            fetch("https://hackathon-app-2-wfdy.onrender.com/checkprime", {
+            fetch("https://hackathon-app-9hq6.onrender.com/checkprime", {
                 method: "POST",
                 headers: {
                     "Content-Type": "Application/json",
